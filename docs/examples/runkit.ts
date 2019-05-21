5d49ac5e6beac07edf504c5c55f5e6a61c030d96
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-21 21:02:41 +0200

    Update runkit.ts

--- docs/examples/runkit.ts
+++ docs/examples/runkit.ts
@@ -1,35 +1,38 @@
 const { CommandHandler, Command } = require("vnftjs");
 const axios = require("axios");
 
+// initialisation
 const client = new CommandHandler();
 const Discord_Token = "Discord Token";
 
+// sets the prefix for the bot to !
 client.prefix = "!";
 
+// !ping
 const pingCommand = new Command();
 pingCommand.name = "ping";
 pingCommand.funct = (bot, message, args) => {
   message.reply("Pong!");
 };
 
+// !neko or !cat
 const neko = new Command();
 neko.name = "neko";
 neko.addAlias("cat");
+neko.description = "replies a random cat image with the help of random.cat";
+neko.usage= "";
 neko.funct = async (bot, message, args) => {
   var meow = await axios.get("http://aws.random.cat/meow");
   message.reply(meow.data.file);
 };
 
-const activity = new Command();
-activity.name = "setActivity";
-activity.addAlias("activity");
-activity.funct = async (bot, message, args) => {
-  await bot.user.setActivity(args);
-  message.reply(`Status Updated`);
-};
+// !help
+client.enableHelp();
+client.helpColor = "GREEN";
 
+// this simulates client.loadCommands(path)
 client.addCommand(pingCommand);
 client.addCommand(neko);
-client.addCommand(activity);
+
 
 client.login(Discord_Token);
