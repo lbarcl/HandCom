b79720fd9bcf5c080daf3d42c1af948fd1bcf229
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-03 19:28:53 +0200

    prettier

--- test/testbot.ts
+++ test/testbot.ts
@@ -2,10 +2,10 @@ import * as assert from "assert";
 import { CommandHandler } from "../src";
 import * as path from "path";
 
-describe('setup', async () => {
+describe("setup", async () => {
   const client = new CommandHandler();
-  
-  before( async () => {
+
+  before(async () => {
     client.prefix = "!";
     client.enableDebug();
     client.enableHelp();
@@ -14,26 +14,25 @@ describe('setup', async () => {
     client.loadCommands(path.join(__dirname, "commands"));
     client.loadScripts(path.join(__dirname, "scripts"));
 
-    await client.login(process.env.test_token)
+    await client.login(process.env.test_token);
   });
 
-  it('login success', done => {
+  it("login success", done => {
     assert.notEqual(client, "undefined");
     done();
   });
 
-  it('help command is in', done => {
-    assert.equal(client.commands.some(c=>c.name=="help"), true);
-    done();
-  });
-  
-  it('ping command is in', done => {
-    assert.equal(client.commands.some(c=>c.name=="ping"), true);
+  it("help command is in", done => {
+    assert.equal(client.commands.some(c => c.name == "help"), true);
     done();
   });
 
-  after(()=>{
+  it("ping command is in", done => {
+    assert.equal(client.commands.some(c => c.name == "ping"), true);
+    done();
+  });
+
+  after(() => {
     client.destroy();
-    process.exit();
-  })
+  });
 });
