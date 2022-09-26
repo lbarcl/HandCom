a97e8b1d0dab51d9bb44c5667617e4200c0134cb
Barçın <68463413+lbarcl@users.noreply.github.com>
2022-09-26 23:58:45 +0300

    Delete dist directory

--- dist/cjs/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function fetchJS(folder: string): string[];
-//# sourceMappingURL=utils.d.ts.map
--- dist/es/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as Command from './command';
-import * as Event from './event';
-import { fetchJS } from './utils';
-import { CommandHandler } from './commander';
-export { Command, CommandHandler, fetchJS, Event };
-//# sourceMappingURL=index.d.ts.map
