39b2c8fcc49a69067fb68b90b354eb9fe1a7daea
Barçın <ersoybarcin@gmail.com>
2023-07-04 13:19:47 +0300

    V2.0.2 -> Utils

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,21 @@ import { Command } from './command';
 import { Event } from './event';
 import { CommandHandler } from './commander';
 import { ApplicationCommand, ApplicationCommandOption, ApplicationCommandOptionChoice } from './ApplicationCommand';
-
+import { COMMAND_TYPES, COMMAND_OPTION_TYPES, LANGUAGES, CHANNEL_TYPES } from './utils';
+const Utils = {
+    CHANNEL_TYPES,
+    COMMAND_TYPES,
+    COMMAND_OPTION_TYPES,
+    LANGUAGES
+}
 export {
     ApplicationCommand,
     ApplicationCommandOption,
     ApplicationCommandOptionChoice,
     Command,
     Event,
-    CommandHandler
+    CommandHandler,
+    Utils
 };
 
 export default CommandHandler;
